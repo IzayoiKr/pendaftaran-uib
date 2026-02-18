d04f97ec1cff2f3d389eb3d6e30b2ef280ecb563
IzayoiKr <izayoilv@gmail.com>
2026-02-18 20:32:14 +0700

    Add minor config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     server: {
+        host: true,
+        port: 8989,
         proxy: {
             '/api': 'http://localhost:8888',
         },
