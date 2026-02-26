3f53ff63a95f8a1cd84571ad74e3c85fb1fdd76c
IzayoiKr <izayoilv@gmail.com>
2026-02-26 21:17:14 +0700

    Fix import logo

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,28 +1,26 @@
-import type { FC } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
+import logo from '../../assets/logo.png';
+import { navLinks } from '../../constants/navigation';
 
-const Header: FC = () => {
+export default function Header() {
     return (
         <header className={styles.header}>
             <div className="container">
                 <nav className={styles.nav}>
                     <Link to="/" className={styles.logo}>
-                        <img src="../../assets/logo.png" alt="UIB" />
+                        <img src={logo} alt="UIB" />
                     </Link>
                     <ul className={styles.menu}>
-                        <li><Link to="/#home">Beranda</Link></li>
-                        <li><Link to="/#gelombang">Gelombang</Link></li>
-                        <li><Link to="/#info-umum">Informasi Umum</Link></li>
-                        <li><Link to="/#kontak">Kontak</Link></li>
-                        <li><Link to="/#panduan">Panduan</Link></li>
-                        <li><Link to="/#login">Daftar/Login</Link></li>
+                        {navLinks.map(link => (
+                            <li key={link.to}>
+                                <Link to={link.to}>{link.label}</Link>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
             </div>
         </header>
     );
 }
-
-export default Header;
 
