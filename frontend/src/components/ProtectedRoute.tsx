2859e291354a7cad2071f5278f37bb7091aba339
TheRicers <thericersjr@gmail.com>
2026-04-11 15:14:28 +0700

    Addressed review comments

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,26 +1,19 @@
 import { useEffect, useRef } from "react";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import useAuthStore from "../store/useAuthStore";
 import { toast } from "sonner";
 
 export default function ProtectedRoute() {
     const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
     const isLoading = useAuthStore((state) => state.isLoading);
-    
     const hasShownToast = useRef(false);
-    const wasAuthenticated = useRef(isAuthenticated);
 
     useEffect(() => {
-        // Only show the warning if they were NEVER authenticated during this session.
-        // If wasAuthenticated.current is true, it means they just clicked "Logout", so skip the warning.
-        if (!isLoading && !isAuthenticated && !hasShownToast.current && !wasAuthenticated.current) {
-            toast.warning("Mohon untuk Login terlebih dahulu!");
+        if (!isLoading && !isAuthenticated && !hasShownToast.current) {
+            toast.warning("Mohon untuk Login terlebih dahulu!")
             hasShownToast.current = true;
         }
-
-        // Keep track of their previous auth state
-        wasAuthenticated.current = isAuthenticated;
-    }, [isLoading, isAuthenticated]);
+    }, [isLoading, isAuthenticated])
 
     if (isLoading) return null;
     if (!isAuthenticated) return <Navigate to='/login' replace />;
